6d7fb2c866031c329d99bb8e75cde76bf8a17193
DevRajeev <2410030112@klh.edu.in>
2025-11-11 14:57:38 +0530

    fix: Pass llm_engine_name to Executor

--- quick_start.py
+++ quick_start.py
@@ -1,8 +1,11 @@
 # Import the solver
 from agentflow.agentflow.solver import construct_solver
 
-# Set the LLM engine name
-llm_engine_name = "dashscope" # you can use "gpt-4o" as well
+# Set the LLM engine name.
+# Supported engines are "dashscope" and "gpt-4o".
+# By default, it uses "dashscope".
+# To use "gpt-4o", uncomment the line below and comment out the "dashscope" line.
+llm_engine_name = "dashscope"
 # llm_engine_name = "gpt-4o"
 
 # Construct the solver
