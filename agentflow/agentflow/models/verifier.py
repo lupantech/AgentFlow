d68b706f8f4911ce421d1e2d6f50ee83ff26e70e
TaeBbong <mok05289@korea.ac.kr>
2026-01-27 18:14:33 -0800

    feat: introduce base_url_fixed for local model - Add field `base_url_fixed` to setup base_url for llm_engine_fixed - Use field `base_url_fixed` for each flow modules

--- agentflow/agentflow/models/verifier.py
+++ agentflow/agentflow/models/verifier.py
@@ -13,12 +13,12 @@ from agentflow.models.memory import Memory
 class Verifier:
     def __init__(self, llm_engine_name: str, llm_engine_fixed_name: str = "dashscope",
                  toolbox_metadata: dict = None, available_tools: list = None,
-                 verbose: bool = False, base_url: str = None, is_multimodal: bool = False,
+                 verbose: bool = False, base_url: str = None, base_url_fixed: str = None, is_multimodal: bool = False,
                  check_model: bool = True, temperature: float = .0):
         self.llm_engine_name = llm_engine_name
         self.llm_engine_fixed_name = llm_engine_fixed_name
         self.is_multimodal = is_multimodal
-        self.llm_engine_fixed = create_llm_engine(model_string=llm_engine_fixed_name, is_multimodal=False, temperature=temperature)
+        self.llm_engine_fixed = create_llm_engine(model_string=llm_engine_fixed_name, is_multimodal=False, base_url=base_url_fixed, temperature=temperature)
         self.llm_engine = create_llm_engine(model_string=llm_engine_name, is_multimodal=False, base_url=base_url, temperature=temperature)
         self.toolbox_metadata = toolbox_metadata if toolbox_metadata is not None else {}
         self.available_tools = available_tools if available_tools is not None else []
